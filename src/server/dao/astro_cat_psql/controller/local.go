dc4b53e40ca40fc565bfc5757f9074b4cb812bc0
DarkPride28 <tecnosebas28@gmail.com>
2025-05-15 23:05:26 -0500

    Agregando en controller dao local.go

---
+++ src/server/dao/astro_cat_psql/controller/local.go
@@ -0,0 +1,115 @@
+package controller
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"onichankimochi.com/astro_cat_backend/src/logging"
+	"onichankimochi.com/astro_cat_backend/src/server/dao/astro_cat_psql/model"
+)
+
+type Local struct{
+	logger       logging.Logger
+	PostgresqlDB *gorm.DB
+}
+
+// Create Local postgresql controller
+func NewLocalController(logger logging.Logger, postgresqlDB *gorm.DB) *Local{
+	return &Local{
+		logger:       logger,
+		PostgresqlDB: postgresqlDB,
+	}
+}
+
+// Gets a local model given params.
+func (p *Local) GetLocal(localId uuid.UUID) (*model.Local, error) {
+	local := &model.Local{}
+
+	result := p.PostgresqlDB.First(&local, "id = ?", localId)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return local, nil
+}
+// Fetch all locals.
+func (p *Local) FetchLocals() ([]*model.Local, error) {
+	locals := []*model.Local{}
+
+	result := p.PostgresqlDB.Find(&locals)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return locals, nil
+}
+// Creates a professional given its model.
+func (p *Local) CreateLocal(local *model.Local) error {
+	return p.PostgresqlDB.Create(local).Error
+}
+
+// Updates a local given its model.
+func (p *Local) UpdateLocal(
+	id uuid.UUID,
+	localName *string,
+	streetName *string,
+	buildingNumber *string,
+	district *string,
+	province *string,
+	region *string,
+	reference *string,
+	capacity *int,
+	imageUrl *string,
+	updatedBy string,
+) (*model.Local, error) {
+	updateFields := map[string]any{
+		"updated_by": updatedBy,
+	}
+	if localName != nil{
+		updateFields["local_name"] = *localName
+	}
+	if streetName != nil{
+		updateFields["street_name"] = *streetName
+	}
+	if buildingNumber != nil{
+		updateFields["building_number"] = *buildingNumber
+	}
+	if district != nil{
+		updateFields["district"] = *district
+	}
+	if province != nil{
+		updateFields["province"] = *province
+	}
+	if region != nil{
+		updateFields["region"] = *region
+	}
+	if reference != nil{
+		updateFields["reference"] = *reference
+	}
+	if capacity != nil{
+		updateFields["capacity"] = *capacity
+	}
+	if imageUrl != nil{
+		updateFields["image_url"] = *imageUrl
+	}
+	// Check if there are any fields to update
+	var local model.Local;
+	if len(updateFields) == 1 {
+		if err := p.PostgresqlDB.First(&local, "id = ?", id).Error; err != nil {
+			return nil, err
+		}
+		return &local,nil;
+	}
+	// Perform the update
+	result := p.PostgresqlDB.Model(&local).
+		Clauses(clause.Returning{}).
+		Where("id = ?", id).
+		Updates(updateFields)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return nil, gorm.ErrRecordNotFound
+	}
+	return &local,nil;
+}
