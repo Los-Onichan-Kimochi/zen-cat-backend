8600e046b47e29b57a55ef4d19d7a7725081b8fa
SereSoyer <dromo1705@outlook.com>
2025-06-06 02:08:25 -0500

    minor change

--- src/server/utils/password.go
+++ src/server/utils/password.go
@@ -2,6 +2,7 @@ package utils
 
 import "golang.org/x/crypto/bcrypt"
 
+// hash y confirmacion
 func HashPassword(plain string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(plain), bcrypt.DefaultCost)
 	return string(bytes), err
