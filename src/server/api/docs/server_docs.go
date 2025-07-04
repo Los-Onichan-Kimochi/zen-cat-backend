929f6b310c4c67e73cce217db9773b9f8f4b4068
D4rkPatata <a20211041@pucp.edu.pe>
2025-07-04 18:53:10 -0500

    adding +1 -1 reservations_used logic

--- src/server/api/docs/server_docs.go
+++ src/server/api/docs/server_docs.go
@@ -10836,12 +10836,16 @@ const docTemplateserver = `{
         "time.Duration": {
             "type": "integer",
             "enum": [
+                -9223372036854775808,
+                9223372036854775807,
                 1,
                 1000,
                 1000000,
                 1000000000,
                 60000000000,
                 3600000000000,
+                -9223372036854775808,
+                9223372036854775807,
                 1,
                 1000,
                 1000000,
@@ -10850,12 +10854,16 @@ const docTemplateserver = `{
                 3600000000000
             ],
             "x-enum-varnames": [
+                "minDuration",
+                "maxDuration",
                 "Nanosecond",
                 "Microsecond",
                 "Millisecond",
                 "Second",
                 "Minute",
                 "Hour",
+                "minDuration",
+                "maxDuration",
                 "Nanosecond",
                 "Microsecond",
                 "Millisecond",
